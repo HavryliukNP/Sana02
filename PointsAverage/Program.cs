1ece884096e3e84bf96c2f0ff81ceb0f68f540f4
Nazarii Havryliuk <ipz234_gnp@student.ztu.edu.ua>
2023-11-20 14:05:47 +0200

    Додав 1 та 2 завдання

---
+++ PointsAverage/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введіть 5 оцінок за 100-бальною шкалою");
+
+int sum = 0;
+
+for (int i = 0; i < 5; i++)
+{
+    int a = int.Parse(Console.ReadLine());
+    sum += a;
+}
+if((sum / 5)>=50)
+    Console.WriteLine("Студент отримує допуск");
+else Console.WriteLine("Студент не отримує допуск");
+
---
+++ QuadraticEquation/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Text;
+Console.InputEncoding = Encoding.Unicode;
+Console.OutputEncoding = Encoding.Unicode;
+
+Console.WriteLine("Розв’язання квадратного рівняння (ax^2 + bx + c = 0)");
+
+Console.WriteLine("Введіть a:");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введіть b:");
+int b = int.Parse(Console.ReadLine());
+Console.WriteLine("Введіть c:");
+int c = int.Parse(Console.ReadLine());
+
+double D, x1, x2;
+
+D = Math.Pow(b, 2) - 4 * a * c;
+Console.WriteLine($"D = {D}");
+
+if (D == 0) 
+{ 
+    x1 = -b / (2 * a);
+    Console.WriteLine($"x = {x1}");
+}
+else if (D > 0)
+{
+    x1 = (-b + Math.Sqrt(D))/ (2 * a);
+    Console.WriteLine($"x1 = {x1}");
+    x2 = (-b - Math.Sqrt(D)) / (2 * a);
+    Console.WriteLine($"x2 = {x2}");
+}else Console.WriteLine("Розв’язків немає");
